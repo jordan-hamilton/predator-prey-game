e803df78d030d46393f51124506da46e51f9fdc9
sarahflower <35937696+sarahflower@users.noreply.github.com>
2018-10-14 15:18:55 -0700

    Create Critter.hpp

---
+++ Critter.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#ifndef CRITTER_HPP
+#define CRITTER_HPP
+
+class Critter
+{
+private:
+
+public:
+
+};
+#endif
+
+/*Create a class named Critter that contains data and functions common to ants and doodlebugs. 
+This class should have a virtual function named move that is defined in the derived classes of 
+Ant and Doodlebug.  Each class will be in its own source file. */
