a80e98674c9d2fe44d499ad805732fee4c6276d7
Jake Anderson <44146856+anderja8@users.noreply.github.com>
2018-10-18 17:16:57 -0400

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -1,5 +1,20 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
+#include "Critter.hpp"
+#include "Doodlebug.hpp"
+#include "Ant.hpp"
 
-class Board:
+class Board {
 private:
+  int numRows;
+  int numCols;
+  int numAnts;
+  int numDoodleBugs;
+  Critter** bugBoard;
+  
+public:
+  Board();
+  Board(int row, int col, int ant, int doodleBug);
+  void placeDoodleBug();
+  void placeAnt();
+  void moveDoodleBugs
