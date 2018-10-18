5857694c47e394331a326aa0086f7e71c62ad700
Jake Anderson <44146856+anderja8@users.noreply.github.com>
2018-10-18 17:25:50 -0400

    Create Board.cpp

---
+++ Board.cpp
@@ -0,0 +1,76 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+#include "Critter.hpp"
+#include "Doodlebug.hpp"
+#include "Ant.hpp"
+
+class Board {
+private:
+  int numRows;
+  int numCols;
+  int numAnts;
+  int numDoodlebugs;
+  Critter** bugBoard;
+  
+public:
+
+//default constructor, sets private members to standard values and creates board
+Board() {
+  numRows = 20;
+  numCols = 20;
+  numAnts = 100;
+  numDoodlebugs = 5;
+
+  //dynamically allocate the bugBoard
+  bugBoard = new Critter*[numRows];
+  for (int i = 0; i < numRows; i++) {
+    Critter[i] = new Critter[numCols];
+  }
+  
+  //place the ants
+  for (int i = 0; i < numAnts; i++) {
+    this->placeAnts();
+  }
+  
+  //place the doodlebugs
+  for (int i = 0; i < numAnts; i++) {
+    this->placeDoodlebugs();
+  }
+}
+  
+Board(int row, int col, int ant, int doodlebug) {
+  numRows = row;
+  numCols = col;
+  numAnts = ant;
+  numDoodlebugs = doodlebug;
+
+  //dynamically allocate the bugBoard
+  bugBoard = new Critter*[numRows];
+  for (int i = 0; i < numRows; i++) {
+    Critter[i] = new Critter[numCols];
+  }
+  
+  //place the ants
+  for (int i = 0; i < numAnts; i++) {
+    this->placeAnts();
+  }
+  
+  //place the doodlebugs
+  for (int i = 0; i < numAnts; i++) {
+    this->placeDoodlebugs();
+  }
+}
+  
+  void placeDoodlebug();
+  
+  void placeAnts();
+  
+  void moveDoodlebugs();
+  
+  void breedDoodlebugs();
+  
+  void moveAnts();
+  
+  void breedAnts();
+  
+  void printBoard();
