96c3582e7600c2dec5803620bee1e4a997a815d7
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-10-29 22:17:36 -0600

    Include Ant.hpp for eat function?

--- Doodlebug.hpp
+++ Doodlebug.hpp
@@ -1,6 +1,7 @@
 #ifndef DOODLEBUG_HPP
 #define DOODLEBUG_HPP
 
+#include "Ant.hpp"
 #include "Critter.hpp"
 
 class Doodlebug : public Critter
@@ -8,7 +9,7 @@ class Doodlebug : public Critter
 private:
   int lastMeal;
   bool isStarved;
-  
+
 public:
   Doodlebug();
   Doodlebug(int r, int c); // ints - row and column location of Doodlebug
@@ -20,22 +21,23 @@ public:
   void incrementLastMeal();   // lastMeal++
   int getLastMeal();
   void setLastMeal(int);
-  
+
   //print the type of bug (for use in print board function)
-  virtual void printType();
-  
+  void printType() override;
+
   //return a 0 for doodlebug and 1 for ant (for use in movement and eating functions? We may just get rid of this one)
   int returnType() override;
 };
+
 #endif
 
 /*The doodlebugs behave according to the following model:
-Move: For every time step, the doodlebug will firstly try to move to an adjacent cell 
-containing an ant and eat the ant (you can decide if there are several ants in the adjacent cells, 
-how the doodlebug will choose to move). If there are no ants in adjacent cells, the doodlebug moves 
+Move: For every time step, the doodlebug will firstly try to move to an adjacent cell
+containing an ant and eat the ant (you can decide if there are several ants in the adjacent cells,
+how the doodlebug will choose to move). If there are no ants in adjacent cells, the doodlebug moves
 according to the same rules as the ant.  Note that a doodlebug cannot eat other doodlebugs.
 
-Breed: If a doodlebug survives for eight time steps, at the end of the time step, it will spawn off 
+Breed: If a doodlebug survives for eight time steps, at the end of the time step, it will spawn off
 a new doodlebug in the same manner as the ant (the Doodlebug will only breed into an empty cell).
 
 Starve: If a doodlebug has not eaten an ant within three time steps, at the end of the third time
