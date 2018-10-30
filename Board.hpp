402bd3269386e1a166559d47b2ab3f9cc105d72f
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-10-29 22:21:39 -0600

    Forward declaration of Critter class

--- Board.hpp
+++ Board.hpp
@@ -1,34 +1,44 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
-#include "Critter.hpp"
-#include "Doodlebug.hpp"
-#include "Ant.hpp"
+
+class Critter;
 
 class Board {
-private:
-  int numRows;
-  int numCols;
-  int numAnts;
-  int numDoodlebugs;
-  Critter*** bugBoard;
-  
-public:
-  Board();
-  
-  Board(int row, int col, int ant, int doodlebug);
-  
-  void placeDoodlebug();
-  
-  void placeAnts();
-  
-  void placeBug(int species);
-  
-  void moveAnts();
-  
-  void breedAnts();
-  
-  void printBoard();
-  
-  Critter* getContents(int row, int col);
+
+  private:
+    int numRows;
+    int numCols;
+    int numAnts;
+    int numDoodlebugs;
+    Critter*** bugBoard;
+
+  public:
+    Board();
+
+    Board(int row, int col, int ant, int doodlebug);
+
+    void placeDoodlebug();
+
+    void placeAnts();
+
+    void placeBug(int species);
+
+    void moveDoodlebugs();
+
+    void breedDoodlebugs();
+
+    void moveAnts();
+
+    void breedAnts();
+
+    void printBoard();
+
+    Critter* getContents(int row, int col);
+
+    int getNumCols();
+    
+    int getNumRows();
+
+};
 
 #endif
