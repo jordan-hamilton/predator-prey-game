f6f11c5083650f5232a5553c6f16dd0eee3f2de2
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-10-29 22:16:46 -0600

    Return an integer for returnType

--- Doodlebug.hpp
+++ Doodlebug.hpp
@@ -25,7 +25,7 @@ public:
   virtual void printType();
   
   //return a 0 for doodlebug and 1 for ant (for use in movement and eating functions? We may just get rid of this one)
-  virtual void returnType();
+  int returnType() override;
 };
 #endif
 
