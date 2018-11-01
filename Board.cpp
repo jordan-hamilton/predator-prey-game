537e64978c4ee303dc79aea9c9105e8adb77d0ca
Jake Anderson <44146856+anderja8@users.noreply.github.com>
2018-11-01 17:05:42 -0400

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -248,3 +248,23 @@ int Board::getNumCols() {
 int Board::getNumRows() {
   return numRows;
 }
+
+void Board::starve() {
+ //loop through the board
+ for (int i = 0; i < numRows; i++) {
+   for (int j = 0; j < numCols; j++) {
+     
+     //check if we're looking at a doodlebug
+     if (bugBoard[i][j] != NULL) {
+       if (bugBoard[i][j]->returnType() == 0) {
+         
+         //check whether the doodlebug starved, delete if true
+         if (bugBoard[i][j]->starve() == true) {
+           delete bugBoard[i][j];
+           bugBoard[i][j] = NULL;
+         }
+       }
+     }
+   }
+ }
+}
