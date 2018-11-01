95914ba0f5cec1af373ca1cd5a02cd103f81ab5d
JQuilty <jquilty@gmail.com>
2018-11-01 18:31:03 -0500

    Update Ant.cpp
    Added isReadtyToBreed and breed functions. Note that this will have to be implemented within the movement functions once ready, we may need to re-evaluate the parameters being passed.

--- Ant.cpp
+++ Ant.cpp
@@ -108,3 +108,75 @@ adjacent (up, down, left, or right) cell that is empty randomly. If the cell tha
 randomly attempt to pick another one.  If there is no empty cell available, no breeding occurs.
 Once an offspring is produced, an ant cannot produce an offspring again until it has
 survived three more time steps. */
+
+
+/*
+ * breed is intended to be run at the end of every turn. If both a pending breeding exists and an adjacent square exists,
+ * it will randomly select a direction to spawn a new ant in. breedingPending is then set to false.
+ *
+ * If either case is false,the function simply returns without modifying anything and the ant will have an opportunity
+ * to breed on it's next turn.
+ */
+void Ant::breed(Board &b)
+{
+	if (breedingPending == true && b.doesAdjacentExist() == true)
+	{
+		int tempDirection;
+		bool hasCompleted = false;
+
+		while (hasCompleted == false)
+		{
+			rand() % 4 + 1 = tempDirection;
+
+			if (tempDirection == 1)
+			{
+				if (b.getContents(row -1, col) == NULL)
+				{
+					new Ant(row -1, col);
+					hasCompleted = true;
+				}
+			}
+
+			else if (tempDirection == 2) //Right
+			{
+				if (b.getContents(row, col + 1) == NULL)
+				{
+					new Ant(row, col + 1);
+					hasCompleted = true;
+				}
+			}
+
+			else if (tempDirection == 3) //Down
+			{
+				if (b.getContents(row + 1, col) == NULL)
+				{
+					new Ant(row + 1, col);
+					hasCompleted = true;
+				}
+			}
+
+			else if (tempDirection == 4) //Left
+			{
+				if (b.getContents(row, col - 1) == NULL)
+				{
+					new Ant(row, col - 1);
+				}
+			}
+		}
+	}
+	else return; //If there is a return, breedingPending will remain true and it will be evaluated again on the next round, giving the ant the opportunity to breed again.
+}
+
+
+
+/*
+ * isReadyToBreed is intended to be run after every turn. It will check to see if the ant's age allows it to breed. If there's already
+ * a pending breeding, the if statement will skip over the operation and the function will end.
+ */
+void Ant::isReadyToBreed()
+{
+	if (age % 3 == 0 && readyToBreed == false)
+	{
+		readyToBreed = true;
+	}
+}
