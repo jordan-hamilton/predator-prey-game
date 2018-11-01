bc3524b8999f99a03a601ca2f52bc97f9667ec0f
JQuilty <jquilty@gmail.com>
2018-11-01 18:27:52 -0500

    Update Critter.cpp
    Added doesFreeAdjacentExist function.

--- Critter.cpp
+++ Critter.cpp
@@ -32,3 +32,37 @@ int getCol() {
 bool starve() {
   return false;
 }
+
+/*
+ * doesFreeAdjacentExist takes a board as a parameter and checks the spaces adjacent to the critter's coordinates.
+ * If any of the spaces are free, it'll immediately return true regardless of the number of spaces. It will only return false
+ * if all four have been checked and shown to not be empty.
+ */
+bool Critter::doesFreeAdjacentExist(Board &b)
+{
+	//Checking up
+	if (b.getContents(this->row -1, this->col) == NULL)
+	{
+		return true;
+	}
+
+	//Checking Right
+	else if (b.getContents(row, col + 1) == NULL)
+	{
+		return true;
+	}
+
+	//Down
+	else if (b.getContents(row + 1, col) == NULL)
+	{
+		return true;
+	}
+
+	//Left
+	else if (b.getContents(row, col - 1) == NULL)
+	{
+		return true;
+	}
+
+	else return false;
+}
