5305604f0b6ad0a8783856838139e90d7eab380e
tonthdan <44146949+tonthdan@users.noreply.github.com>
2018-11-01 23:51:02 -0700

    Update Ant.cpp

--- Ant.cpp
+++ Ant.cpp
@@ -32,56 +32,57 @@ Ant::~Ant()
 *********************************************************************/
 void Ant::move(Critter*** board, int row, int col)
 {
-    // 1 = RIGHT, 2 = LEFT, 3 = UP, 4 = DOWN
-    int direction = rand()% 4 + 1;
+	// 1 = RIGHT, 2 = LEFT, 3 = UP, 4 = DOWN
+	int direction = rand() % 4 + 1;
 
-    switch (direction)
-    {
-        case 1:     // Ant moving right
-            if((col + 1 >= 0) && (col + 1 < 20))
-            {
-                if (board.getContents(row, col + 1) == NULL)
-                {
-                    col += 1;
-                    age++;
-                }
-            }
-            break;
+	switch (direction)
+	{
+	case 1:     // Ant moving right
+		if ((col + 1 >= 0) && (col + 1 < 20))
+		{
+			if (board[row][col+1] == NULL)
+			{
+				col += 1;
+				age++;
+			}
+		}
+		break;
 
-        case 2:     // Ant moving left
-            if((col - 1 >= 0) && (col - 1 < 20))
-            {
-                if (board.getContents(row, col - 1) == NULL)
-                {
-                    col -= 1;
-                    age++;
-                }
-            }
-            break;
+	case 2:     // Ant moving left
+		if ((col - 1 >= 0) && (col - 1 < 20))
+		{
+			if (board[row][ col - 1] == NULL)
+			{
+				col -= 1;
+				age++;
+			}
+		}
+		break;
 
-        case 3:     // Ant moving up
-            if((row - 1 >= 0) && (row - 1 < 20))
-            {
-                if (board.getContents(row - 1, col) == NULL)
-                {
-                    row -= 1;
-                    age++;
-                }
-            }
-            break;
+	case 3:     // Ant moving up
+		if ((row - 1 >= 0) && (row - 1 < 20))
+		{
+			if (board[row - 1][ col] == NULL)
+			{
+				row -= 1;
+				age++;
+			}
+		}
+		break;
 
-        case 4:     // Ant moving down
-            if((row + 1 >= 0) && (row + 1 < 20))
-            {
-                if(board.getContents(row + 1, col) == NULL)
-                {
-                    row += 1;
-                    age++;
-                }
-            }
-            break;
-    }
+	case 4:     // Ant moving down
+		if ((row + 1 >= 0) && (row + 1 < 20))
+		{
+			if (board[row + 1][ col] == NULL)
+			{
+				row += 1;
+				age++;
+			}
+		}
+		break;
+	}
 }
+
 
 void Ant::breed(Critter*** board)
 {
